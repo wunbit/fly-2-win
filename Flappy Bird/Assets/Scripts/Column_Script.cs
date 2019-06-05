8bb973cfe2e6b45a681d0a1bd1102f97e022d96b
Soleyu <soleyu.alpha@gmaill.com>
2019-06-05 18:21:08 -0500

    Agregando scoring

---
+++ Flappy Bird/Assets/Scripts/Column_Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column_Script : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.GetComponent<Bird_Scrpt>() != null)
+         {
+             GameControl.instance.BirdScored ();
+         }
+    }
+}
